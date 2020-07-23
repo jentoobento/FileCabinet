5651f0d8527309a87668d48578c76d7a407a9bcf
enlei <ray556657@gmail.com>
2020-07-23 16:39:32 -0700

    add-new-list-modal

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,19 +1,68 @@
-import React from 'react';
-import {View, Pressable} from 'react-native';
-import {Alert} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Pressable,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import {Modal} from 'react-native';
 import {Icon} from 'react-native-elements';
 import COLORS from '../../resources/colors';
 import styles from './styles';
 
 const Home = () => {
-  const buttonClick = () => {
-    Alert.alert("I'm clicked broh!");
+  const [modalShow, setModalShow] = useState(false);
+  const [listName, changeListName] = useState('');
+
+  const addListClick = () => {
+    setModalShow(true);
+  };
+
+  const listModalDismiss = () => {
+    setModalShow(false);
+    changeListName('');
   };
 
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalShow}
+        onRequestClose={() => listModalDismiss()}>
+        <TouchableOpacity
+          style={styles.centeredView}
+          activeOpacity={1}
+          onPress={() => {
+            listModalDismiss();
+          }}>
+          <TouchableWithoutFeedback>
+            <View style={styles.modalView}>
+              <Pressable style={styles.addIcon}>
+                <Text style={styles.textStyle}>Add Icon</Text>
+              </Pressable>
+              <View style={styles.addListNameBorder}>
+                <TextInput
+                  style={styles.addListName}
+                  onChangeText={(text) => changeListName(text)}
+                  placeholder="Add List Name"
+                  placeholderTextColor={COLORS.lightBlue}
+                  value={listName}
+                />
+              </View>
+              <Pressable style={styles.createButton} disabled={true}>
+                <Text style={{...styles.textStyle, color: COLORS.grey}}>
+                  Create
+                </Text>
+              </Pressable>
+            </View>
+          </TouchableWithoutFeedback>
+        </TouchableOpacity>
+      </Modal>
       <View style={styles.buttonContainer}>
-        <Pressable onPress={buttonClick} style={styles.button}>
+        <Pressable onPress={addListClick} style={styles.button}>
           <Icon name="plus" type="entypo" color={COLORS.grey} size={36} />
         </Pressable>
       </View>

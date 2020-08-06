0451f0b4107a3c887bb1a19980803b3aaaf026d3
enlei <ray556657@gmail.com>
2020-08-06 09:45:06 -0700

    reorder-strings

--- src/resources/strings.js
+++ src/resources/strings.js
@@ -2,9 +2,9 @@ import LocalizedStrings from 'react-native-localization';
 
 const strings = new LocalizedStrings({
   en: {
+    add_file: 'Add New File',
     add_icon: 'Add Icon',
     add_list_name: 'Add List Name',
-    add_file: 'Add New File',
     create: 'Create',
   },
 });
